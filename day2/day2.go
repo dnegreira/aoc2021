52fca0af9d6f6c9082ffdb0d15243a62b0bff661
David Negreira <david@otherreality.net>
2021-12-06 20:58:22 +0100

    Refactor day2
    Removed redundant function and made it only into one.
    
    Signed-off-by: David Negreira <david@otherreality.net>

--- day2/day2.go
+++ day2/day2.go
@@ -17,13 +17,12 @@ func main() {
 		log.Fatal(err)
 	}
 	rawData = advent.LoadInput(inputFile)
-
-	fmt.Println("Answer for problem1 is: ", findDepth(rawData))
-	fmt.Println("Answer for problem2 is: ", findDepthWithAim(rawData))
+	answer1, answer2 := findDepth(rawData)
+	fmt.Printf("Answers are for problem1: %d, for problem2: %d\n",answer1, answer2)
 }
 
-func findDepth(data []string) int {
-	var distance, depth int
+func findDepth(data []string) (int, int) {
+	var distance, depth, aim int
 	for _, v := range data {
 		order := strings.Fields(v)[0]
 		value, err := strconv.Atoi(strings.Fields(v)[1])
@@ -34,33 +33,12 @@ func findDepth(data []string) int {
 		switch order {
 		case "forward":
 			distance += value
+			aim += depth * value
 		case "up":
 			depth -= value
 		case "down":
 			depth += value
 		}
 	}
-	return distance * depth
-}
-
-func findDepthWithAim(data []string) int {
-	var distance, depth, aim int
-	for _, v := range data {
-		order := strings.Fields(v)[0]
-		value, err := strconv.Atoi(strings.Fields(v)[1])
-		if err != nil {
-			log.Fatal("Error converting string to int: ", err)
-		}
-
-		switch order {
-		case "forward":
-			distance += value
-			depth += aim * value
-		case "up":
-			aim -= value
-		case "down":
-			aim += value
-		}
-	}
-	return distance * depth
+	return distance * depth, distance * aim
 }
